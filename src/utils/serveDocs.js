46909871e29cfe3d7f1904e337cf07dc75b642c8
Facundo Creus <fcreus@netrixllc.com>
2023-11-09 22:27:37 -0300

    changed swagger port

--- src/utils/serveDocs.js
+++ src/utils/serveDocs.js
@@ -3,7 +3,7 @@ const swaggerUi = require('swagger-ui-express');
 const YAML = require('yamljs');
 
 const app = express();
-const port = 3000;
+const port = 8080;
 
 const swaggerDocument = YAML.load('./docs/fake-backend-demo.yml');
 
